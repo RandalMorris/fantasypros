70604884f6a53d7592eaa973c6269d0871229a84
RandalMorris <morrrand@gmail.com>
2021-07-03 22:42:03 -0500

    Update rankings.R

--- R/rankings.R
+++ R/rankings.R
@@ -48,38 +48,7 @@ fp_draft_rankings <- function(pos = c("ALL","QB","RB","WR","TE"), scoring = c("H
   if (exists("scoring") == F) scoring <- "ALL" else scoring <- toupper(scoring)
   if (exists("type") == F) type <- "draft" else type <- tolower(type)
   
-  #url = paste0("https://api.fantasypros.com/v2/json/nfl/",year,"/consensus-rankings?type=",type,
-  #             "&scoring=",scoring,"&position=",pos,"&week=",week,"&experts=available")
-  #user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-  #x_api_key="zjxN52G3lP4fORpHRftGI2mTU8cTwxVNvkjByM3j"
-  #authorization = "Basic WC1BcGktS2V5OnpqeE41MkczbFA0Zk9ScEhSZnRHSTJtVFU4Y1R3eFZOdmtqQnlNM2o="
-  
-  fp_auth = fp_auth()
-  fp_url <- fp_build_url(year = year, type = type, scoring = scoring, pos = pos, week = week)
-    
-  res = httr::GET(fp_url,httr::add_headers("user-agent" = fp_auth$user_agent,
-                                  "x-api-key" =  fp_auth$x_api_key,
-                                  "Authorization" =  fp_auth$authorization))
-  
-  raw_data <- httr::content(res, "parsed", "application/json") 
-  raw_data <- raw_data[["players"]]
-  data <- data.frame(t(sapply(raw_data,c)))
-  data <- janitor::clean_names(data) %>% as_tibble()
-  return(data)
+  fp_ranking_data <- fp_get_ranking_data(pos, scoring, year, type, week)
   
   
-}
-
-'
-data_ecr <- fp_draft_rankings(pos="QB") %>% 
-  dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
-         ecr_tier = tier,  ecr_pos_rank = pos_rank, ecr_rank = rank_ecr, ecr_min = rank_min, ecr_ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std)
-data_ecr <- as.data.frame(lapply(data_ecr,unlist))
-
-data_adp <- fp_draft_rankings(pos="QB", type="adp") %>% 
-  dplyr::select(id = player_id,
-         adp_tier = tier,  adp_pos_rank = pos_rank, adp_rank = rank_ecr, adp_min = rank_min, adp_max = rank_max, adp_rank_avg = rank_ave, adp_rank_std =rank_std)
-data_adp <- as.data.frame(lapply(data_adp,unlist))
-
-
-fp_draft_rankings <- dplyr::inner_join(data_ecr, data_adp, by = "id")'
+ }
