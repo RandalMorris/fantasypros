36123e62a82f398af09d01d2295949846940cd8f
RandalMorris <morrrand@gmail.com>
2021-07-03 22:42:13 -0500

    Update fp-urls.R

--- R/fp-urls.R
+++ R/fp-urls.R
@@ -17,38 +17,57 @@ fp_build_url <- function(base = "https://api.fantasypros.com/v2/json/nfl/", year
                "&scoring=",scoring,"&position=",pos,"&week=",week,"&experts=available")
 }
 
-fp_get_ranking_data <- function(pos, type) {
-  if (type == "both")  {
-    data_ecr <- fp_draft_rankings(pos=pos, type="draft") %>% 
+fp_get_ranking_data <- function(year, type, scoring, pos, week) {
+  fp_auth = fp_auth()
+    if (type == "both")  {
+    fp_url <- fp_build_url(year = year, type = "draft", scoring = scoring, pos = pos, week = week)
+    data_ecr <- httr::GET(fp_url,httr::add_headers("user-agent" = fp_auth$user_agent,
+                                                   "x-api-key" =  fp_auth$x_api_key,
+                                                   "Authorization" =  fp_auth$authorization)) %>% 
+      httr::content("parsed", "application/json") %>% .[["players"]] %>% 
+      sapply(c) %>% t() %>% data.frame() %>% janitor::clean_names() %>% as_tibble() %>% 
       dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
       ecr_tier = tier,  ecr_pos_rank = pos_rank, ecr_rank = rank_ecr, ecr_min = rank_min, 
-      ecr_ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std) %>%
+      ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std) %>%
       lapply(unlist) %>% 
       as.data.frame()
 
-    data_adp <- fp_draft_rankings(pos=pos, type="adp") %>% 
+    data_adp <- httr::GET(fp_url,httr::add_headers("user-agent" = fp_auth$user_agent,
+                                                   "x-api-key" =  fp_auth$x_api_key,
+                                                   "Authorization" =  fp_auth$authorization)) %>% 
+      httr::content("parsed", "application/json") %>% .[["players"]] %>% 
+      sapply(c) %>% t() %>% data.frame() %>% janitor::clean_names() %>% as_tibble() %>% 
       dplyr::select(id = player_id,
       adp_tier = tier,  adp_pos_rank = pos_rank, adp_rank = rank_ecr, adp_min = rank_min, 
-      adp_max = rank_max, adp_rank_avg = rank_ave, adp_rank_std =rank_std) %>%
+      adp_max = rank_max, adp_rank_avg = rank_ave, adp_rank_std = rank_std) %>%
       lapply(unlist) %>% 
-        as.data.frame()
+      as.data.frame()
 
-      fp_draft_rankings <- dplyr::inner_join(data_ecr, data_adp, by = "id")
+    fp_draft_rankings <- dplyr::inner_join(data_ecr, data_adp, by = "id")
 
   } else if (type == "draft") {
-    fp_draft_rankings <- fp_draft_rankings(pos=pos, type="draft") %>% 
-      dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
-      ecr_tier = tier,  ecr_pos_rank = pos_rank, ecr_rank = rank_ecr, ecr_min = rank_min, 
-      ecr_ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std) %>%
-      lapply(unlist) %>% 
-      as.data.frame()
+      fp_url <- fp_build_url(year = year, type = "draft", scoring = scoring, pos = pos, week = week)
+      fp_draft_rankings <- httr::GET(fp_url,httr::add_headers("user-agent" = fp_auth$user_agent,
+                                                              "x-api-key" =  fp_auth$x_api_key,
+                                                              "Authorization" =  fp_auth$authorization)) %>% 
+        httr::content("parsed", "application/json") %>% .[["players"]] %>% 
+        sapply(c) %>% t() %>% data.frame() %>% janitor::clean_names() %>% as_tibble() %>% 
+        dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
+        ecr_tier = tier,  ecr_pos_rank = pos_rank, ecr_rank = rank_ecr, ecr_min = rank_min, 
+        ecr_ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std) %>%
+        lapply(unlist) %>% 
+        as.data.frame()
 
   } else if (type == "adp") {
-    fp_draft_rankings <- fp_draft_rankings(pos=pos, type="adp") %>% 
-      dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
-      ecr_tier = tier,  ecr_pos_rank = pos_rank, ecr_rank = rank_ecr, ecr_min = rank_min, 
-      ecr_ecr_max = rank_max, ecr_rank_avg = rank_ave, ecr_rank_std = rank_std) %>%
-      lapply(unlist) %>% 
-      as.data.frame()
+      fp_draft_rankings <- httr::GET(fp_url,httr::add_headers("user-agent" = fp_auth$user_agent,
+                                                              "x-api-key" =  fp_auth$x_api_key,
+                                                              "Authorization" =  fp_auth$authorization)) %>% 
+        httr::content("parsed", "application/json") %>% .[["players"]] %>% 
+        sapply(c) %>% t() %>% data.frame() %>% janitor::clean_names() %>% as_tibble() %>% 
+        dplyr::select(player = player_name, id = player_id, team = player_team_id, position = player_position_id,
+        adp_tier = tier,  adp_pos_rank = pos_rank, adp_rank = rank_ecr, adp_min = rank_min, 
+        adp_max = rank_max, adp_rank_avg = rank_ave, adp_rank_std = rank_std) %>%
+        lapply(unlist) %>% 
+        as.data.frame()
   }
 }
